cf292945472f249bac2bbaa31e5bdca712394212
tarek-jatri <tarek.jatri@gmail.com>
2022-02-28 16:33:32 +0600

    Meeting Get Payload changed

---
+++ common/formatTimestamp.js
@@ -0,0 +1,17 @@
+//
+function formatTimestamp(timestamp) {
+    const date = timestamp.toISOString().split("T")[0];
+    let hours = timestamp.getHours();
+    let minutes = timestamp.getMinutes();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    const strTime = hours + ':' + minutes + ' ' + ampm;
+    return {
+        date,
+        strTime
+    };
+}
+
+module.exports = formatTimestamp;
