a7fe2b26d4be5279ecf2d38f47dd655e5236c6fb
tarek-jatri <tarek.jatri@gmail.com>
2022-05-18 15:25:02 +0600

    Own plugin creation

--- Common/diff-pluggin.js
+++ Common/diff-pluggin.js
@@ -2,21 +2,23 @@ const {detailedDiff} = require("deep-object-diff");
 let previous, current = {};
 
 const plugin = function (schema) {
-    schema.post('init', doc => {
-        console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-        doc._original = doc.toObject({transform: false});
-        previous = doc.toObject({transform: false});
-
-    })
-
+    schema.pre('findOneAndUpdate', async function () {
+        // console.dir(this);
+        // console.dir(this.model.modelName)
+        previous = await this.model.findOne(this.getQuery()).select({__v: 0}).lean();
+        // console.log(docToUpdate); // The document that `findOneAndUpdate()` will modify
+    });
     schema.post('findOneAndUpdate', doc => {
+        console.log(this)
         // console.log("Entered ", doc)
         current = doc;
-
+        delete current.__v;
+        delete previous.__v;
+        // console.log("current ", current)
         console.log("From findOneAndUpdate current: ", current, "\nprevious ", previous);
-        const before = detailedDiff(current, {});
-        const after = detailedDiff({}, current);
-        console.log("before: ", before, "\nafter: ", after);
+        const before = detailedDiff(current, previous);
+        const after = detailedDiff(previous, current);
+        console.log("before: ", before, "\nafter: ", after,);
     })
 
     schema.post('save', doc => {
